af4c976abf24254ef5e02c24cb8b80dfa61c1bef
Irene <terna.legend@gmail.com>
2024-04-21 14:29:30 +0200

    finished meme generator

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,6 +3,41 @@ import memeImage from "../img/meme.png"
 import { TextField, Button } from '@mui/material';
 
 const Meme = () => {
+
+    const [inputData, setInputData] = React.useState({
+
+        topText: "",
+        bottomText: "",
+        imageUrl: ""
+    })
+
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+
+    function getNewImage() {
+        const randomIndex = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomIndex].url;
+        setInputData(prevState => ({
+            ...prevState,
+            imageUrl: url
+        }))
+    }
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setInputData(prevState => {
+            return {
+                ...prevState,
+                [name]: value
+            }
+        })
+    }
+
     return (
         <div className='main-container'>
             <div className='meme-form'>
@@ -10,18 +45,25 @@ const Meme = () => {
                     label="Top text"
                     variant="outlined"
                     size="small"
+                    onChange={handleChange}
+                    name="topText"
+                    value={inputData.topText}
                 />
                 <TextField id="outlined-basic"
                     label="Bottom text"
                     variant="outlined"
-                    size="small" />
-                <Button className='meme-form-button' variant="outlined" color='secondary' >Primary</Button>
+                    size="small"
+                    onChange={handleChange}
+                    name="bottomText"
+                    value={inputData.bottomText}
+                />
+                <Button className='meme-form-button' variant="outlined" color='secondary' onClick={getNewImage}>Get a new image</Button>
             </div>
 
             <div className='meme'>
-                <img className='meme-image' src={memeImage} />
-                <h2 className='meme-heading top'>test top text</h2>
-                <h2 className='meme-heading bottom'>test bottom text</h2>
+                <img className='meme-image' src={inputData.imageUrl} />
+                <h2 className='meme-heading top'>{inputData.topText}</h2>
+                <h2 className='meme-heading bottom'>{inputData.bottomText}</h2>
             </div>
         </div>
     )
